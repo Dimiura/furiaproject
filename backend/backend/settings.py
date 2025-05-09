2b4353a0675ecf35defdec8b3cf814be1803ef5d
dimiura <di.miura@Hotmail.com>
2025-05-09 14:18:47 -0300

    fixes-deploy-v2

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -109,8 +109,11 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': '/cloudsql/meu-projeto:us-central1:furia-postgres',  # substitua
+        'NAME': 'furia_db',
+        'USER': 'django-user',
+        'PASSWORD': 'Dmj041630##@@',
     }
 }
 
